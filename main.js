634194bc21f6f060b34e42adadd5a2833ef11423
Maxim Dmitriev <maximv.dmitriev@gmail.com>
2019-01-07 17:48:34 +0300

    visual

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-let input = document.getElementById('ball'),
+let input = document.querySelector(".route-item"),
     wrapper = document.querySelector(".route-wrapper");
 
-let wrpapStyle = getComputedStyle(wrapper);
+let wrapStyle = getComputedStyle(wrapper);
+
+console.log(wrapper.offsetTop);
 
 function move(e) {
-  // input.style.left = parseInt(wrpapStyle.marginLeft.slice(0, -2)) +  10 + 'px';
-  input.style.top = e.pageY - input.offsetHeight * 2 + 'px';
+  // input.style.left = parseInt(wrapStyle.marginLeft.slice(0, -2)) +  10 + 'px';
+  // input.style.top = e.pageY - input.offsetHeight * 2 + 'px';
+  input.style.top = e.pageY - wrapper.offsetTop - 5 + 'px';
 }
 
 input.addEventListener('mousedown', (event) => {
-
+  console.log(event);
   move(event);
   input.style.zIndex = 1000; 
 
@@ -19,7 +22,7 @@ input.addEventListener('mousedown', (event) => {
     document.removeEventListener('mousemove', move);
     input.onmouseup = null;
     let h = parseInt(input.style.top.slice(0,-2)); 
-    input.style.top = h - h%30 + 5 + "px";
+    input.style.top = h - h%50 + 5 + "px";
   });
 });
 
