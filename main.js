20d647970216e65e30acfe497cf26129f655f32a
Maxim Dmitriev <maximv.dmitriev@gmail.com>
2019-01-08 23:46:30 +0300

    drag and drop

--- main.js
+++ main.js
@@ -13,13 +13,13 @@ document.addEventListener('DOMContentLoaded', () => {
         constructor(id){
             this.id = id;
             this.point = document.createElement("div");
-            this.content = '<input type="text">\
+            this.content = '<input type="text" class="input">\
                             <button class="close">&times;</button>';
         }
         addPoint(){
             this.point.classList.add("route-item");
             this.point.innerHTML = this.content;
-            this.point.style.top = 7 + 57 * (nums - 1) + "px";
+            this.point.style.top = 7 + 57 * (this.id) + "px";
             this.point.setAttribute("id", `${this.id}`);
 
             wrapper.appendChild(this.point);
@@ -44,12 +44,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     newPoint.addEventListener('click', () => {
-        nums++;
-        stylingWrapper(nums);
-
+        
         let div = new Point(nums);
         div.addPoint();
         pointList.push(div);
+        // console.log(pointList);
 
         let deleteBtn = div.point.querySelector(".close");
 
@@ -63,45 +62,92 @@ document.addEventListener('DOMContentLoaded', () => {
                             for(let j = i; j < pointList.length; j++) {
                                 pointList[j].point.style.top = 7 + 57 * j + "px"; // перемещаем все инпуты, ниже удаленного на одну позицию вверх
                             }
-                            
                         }
                     }
-
                     div.removePoint();
                     nums--;
                     stylingWrapper(nums);
                 }
             });
 
+        nums++;
+        stylingWrapper(nums);
 
-            function move(e) {
-                div.point.style.top = e.pageY - wrapper.offsetTop - 15 + 'px';
-            }
-            
-            div.point.addEventListener('mousedown', (event) => {
+        function move(e) {
+            div.point.style.top = e.pageY - wrapper.offsetTop - 15 + 'px';
+        }
         
-                if(!event.target.classList.contains("close") && nums > 1) {
+        div.point.addEventListener('mousedown', (event) => {
+            // console.log("mousedown");
+            let startHeight = parseInt(div.point.style.top.slice(0,-2));
+    
+            if(!event.target.classList.contains("close") && !event.target.classList.contains("input") && nums > 1) {
 
-                    move(event);
-                    div.point.classList.add("move");
-                    div.point.style.zIndex = 1000; 
-        
-                    document.addEventListener('mousemove', move);
-        
-                    div.point.addEventListener('mouseup', () => {
+                move(event);
+                div.point.classList.add("move");
+                div.point.style.zIndex = 1000; 
+    
+                document.addEventListener('mousemove', move);
+    
+                div.point.addEventListener('mouseup', (event) => {
+                    // console.log(pointList);
+                    // console.log("mouseup");
+                    event.target.style.zIndex = 900;
+                    let targetItem = parseInt(event.target.id);
+                    // console.log(targetItem);
                     document.removeEventListener('mousemove', move);
                     div.point.onmouseup = null;
                     div.point.classList.remove("move");
+
                     let h = parseInt(div.point.style.top.slice(0,-2)); 
-                    div.point.style.top = h - h%57 + 5 + "px";
-                    });
+                    h < 0 ? div.point.style.top = "7px" : div.point.style.top = h - h%57 + 7 + "px";
+                    
+                    let diff = (((h - h%57 + 7) - startHeight) / 57) | 0; // насколько элементов сдвинулись
+                    // console.log(diff);
 
+                    if(diff == 0) h = startHeight;
 
-                } 
-                // else {
-                //     console.log("close");
-                // }
-            });
+                    let temp = pointList[targetItem];
+                    // console.log("targetItem: " + targetItem);
+
+                    if(diff > 0) {
+                        // console.log("plus");
+                        for(let j = targetItem; j < (targetItem + diff); j++){
+                            
+                            // console.log(pointList);
+                            pointList[j] = pointList[j + 1];
+                            pointList[j].id = j;
+                            pointList[j].point.setAttribute("id", `${j}`);
+                            pointList[j].point.style.top = 7 + 57 * j + "px";
+                        }
+                        pointList[targetItem + diff] = temp;
+                        pointList[targetItem + diff].id = targetItem + diff;
+                        pointList[targetItem + diff].point.setAttribute("id", `${targetItem + diff}`);
+                        pointList[targetItem + diff].point.style.top = 7 + 57 * (targetItem + diff) + "px";
+      
+                    }
+
+                    if(diff < 0) {
+                        // console.log("minus");
+                        for(let j = targetItem; j > targetItem + diff; j--){ 
+                            pointList[j] = pointList[j - 1];
+                            pointList[j].id = j;
+                            pointList[j].point.setAttribute("id", `${j}`);
+                            pointList[j].point.style.top = 7 + 57 * j + "px";
+                        
+                        }
+                        pointList[targetItem + diff] = temp;
+                        pointList[targetItem + diff].id = targetItem + diff;
+                        pointList[targetItem + diff].point.setAttribute("id", `${targetItem + diff}`);
+                        pointList[targetItem + diff].point.style.top = 7 + 57 * (targetItem + diff) + "px";
+                        // console.log(pointList);
+                    }
+                });
+            } 
+            // else {
+            //     console.log("close");
+            // }
+        });
 
 
 
